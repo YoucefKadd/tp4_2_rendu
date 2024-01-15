e5f96f1e7b57a1c8cfb02bca91934760ef77842b
Youcef k <youssefkaddour@hotmail.com>
2024-01-15 16:11:02 +0100

    commit 18

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-# test_script.py
-
-import unittest
-from unittest.mock import patch
-from script import main
-
-class TestScript(unittest.TestCase):
-    
-    @patch('builtins.print')
-    def test_main_function(self, mock_print):
-        # Testons si la fonction main affiche correctement le message
-        main()
-        mock_print.assert_called_with("Hello Jon Snow, you know nothing!")
-
-if __name__ == '__main__':
-    unittest.main()
+import os
+if float(os.environ['TEST'])>0:
+    print("OK")
