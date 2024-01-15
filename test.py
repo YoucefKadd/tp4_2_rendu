eb62f8cd9b40caf4f78b98fa1d7cbf7c54729d5c
Youcef k <youssefkaddour@hotmail.com>
2024-01-15 15:56:14 +0100

    commit 14

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ class TestScript(unittest.TestCase):
     
     @patch('builtins.print')
     def test_main_function(self, mock_print):
+        # Testons si la fonction main affiche correctement le message
         main()
-        mock_print.assert_called_with("Hello from your Python script!")
+        mock_print.assert_called_with("Hello Jon Snow, you know nothing!")
 
 if __name__ == '__main__':
     unittest.main()
