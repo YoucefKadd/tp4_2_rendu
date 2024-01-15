ed1be18b67f668690d6d531f9b9e8fb11084981d
Youcef k <youssefkaddour@hotmail.com>
2024-01-15 16:04:14 +0100

    commit 15

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
-# script.py
+# # script.py
 
-def main():
-    print("Hello Jon Snow, you know nothing!")
+# def main():
+#     print("Hello Jon Snow, you know nothing!")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+import os
+a = float(os.environ['test'])
+b = 6
+c = b/a
+print("le resultat est :", c)
